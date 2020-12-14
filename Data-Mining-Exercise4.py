3921cce0421bf762666068a2a0fddcc2c89a3b7b
farshad <farshadnp6@gmail.com>
2020-12-15 00:57:29 +0330

    Update Data-Mining-Exercise4.py

--- Data-Mining-Exercise4.py
+++ Data-Mining-Exercise4.py
@@ -1,12 +1,14 @@
 import keyboard
 while True:
     print("_____________________________Tamrin #4 _________________________________\n\n")
-    MySentence = "farshad,nematpour,25,Ahvaz,Mashhad,Azad-University,Data-Minning"
+    
+    MySentence = "farshad,nematpour,farshad,nematpour,farshad"
     print("Our words are: ",MySentence)
     input("enter for Spliting: ")
 
     MySentence_Splited = MySentence.split(",")
-    print("Word after packaging : ", MySentence_Splited)
+    print("Words after Spliting : ", MySentence_Splited)
+    print("Words after Split + Removing Duplicated values: " ,set(MySentence_Splited) )
 
     print("\n___________________________Farshad NematPour ___________________________\n\n")
     keyboard.wait('q')
